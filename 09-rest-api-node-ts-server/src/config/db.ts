93bfce1574f36fb9cfc991113b6afcbfe77b116d
Piero Dev <pierodev21@gmail.com>
2024-11-16 00:10:51 -0500

    chore : add env database url local

--- 09-rest-api-node-ts-server/src/config/db.ts
+++ 09-rest-api-node-ts-server/src/config/db.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
-process.loadEnvFile();
+import 'dotenv/config'
 const db = new Sequelize(process.env.DATABASE_URL!, {
   models: [__dirname + "/../models/**/*.ts"],
 });
