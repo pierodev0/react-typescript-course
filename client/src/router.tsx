c74d9d62cb88d7b186345ea19edb7ae7895911d3
Piero Dev <pierodev21@gmail.com>
2025-10-21 12:42:33 -0500

    feat: recuperar datos ingresados en el form

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,19 +1,20 @@
 import { createBrowserRouter } from "react-router";
 import Layout from "./layouts/Layout";
 import Products from "./views/Products";
-import NewProduct from "./views/NewProduct";
+import NewProduct, { action as newProductAction } from "./views/NewProduct";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
     children: [
       {
-        index : true,
+        index: true,
         element: <Products />,
       },
       {
-        path : 'productos/nuevo',
-        element : <NewProduct/>
+        path: "productos/nuevo",
+        element: <NewProduct />,
+        action: newProductAction,
       },
     ],
   },
--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -1,6 +1,18 @@
-import { Link } from "react-router";
-
+import { Form, Link, useActionData } from "react-router";
+export async function action({ request }) {
+  const data = Object.fromEntries(await request.formData());
+  let error = "";
+  if (Object.values(data).includes("")) {
+    error = "Todos los campos son obligatorios";
+  }
+  if (error.length) {
+    return error;
+  }
+  return {};
+}
 const NewProduct = () => {
+  const error = useActionData();
+  console.log(error);
   return (
     <>
       <div className="flex justify-between">
@@ -14,6 +26,37 @@ const NewProduct = () => {
           Volver a productos
         </Link>
       </div>
+      <Form method="post" className="mt-10">
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="name">
+            Nombre Producto:
+          </label>
+          <input
+            id="name"
+            type="text"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Nombre del Producto"
+            name="name"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-800" htmlFor="price">
+            Precio:
+          </label>
+          <input
+            id="price"
+            type="number"
+            className="mt-2 block w-full p-3 bg-gray-50"
+            placeholder="Precio Producto. ej. 200, 300"
+            name="price"
+          />
+        </div>
+        <input
+          type="submit"
+          className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
+          value="Registrar Producto"
+        />
+      </Form>
     </>
   );
 };
