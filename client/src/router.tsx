2c295cf10c4a79dd064624a2ed5f01562bd4ec95
Piero Dev <pierodev21@gmail.com>
2025-10-21 12:12:48 -0500

    feat: integrate React Router for client-side navigation

---
+++ client/src/router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router";
+import Layout from "./layouts/Layout";
+import Products from "./views/Products";
+import NewProduct from "./views/NewProduct";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index : true,
+        element: <Products />,
+      },
+      {
+        path : 'productos/nuevo',
+        element : <NewProduct/>
+      },
+    ],
+  },
+]);
