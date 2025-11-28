da183dccacb328a1e248bd631a02dbb904c01ee5
Piero Dev <pierodev21@gmail.com>
2025-11-27 21:02:00 -0500

    feat: crear componente form reusable

--- client/src/views/EditProduct.tsx
+++ client/src/views/EditProduct.tsx
@@ -10,6 +10,7 @@ import {
 import ErrorMessage from "../components/ErrorMessage";
 import { editProduct, getProductsById } from "../services/ProductsService";
 import type { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 export async function loader({ params }: LoaderFunctionArgs) {
   if (params.id !== undefined) {
     const product = await getProductsById(+params.id);
@@ -56,32 +57,7 @@ const EditProduct = () => {
       </div>
       {error && <ErrorMessage>{error}</ErrorMessage>}
       <Form method="post" className="mt-10">
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-            defaultValue={product.name}
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-            defaultValue={product.price}
-          />
-        </div>
+        <ProductForm product={product}/>
         <div className="mb-4">
           <label className="text-gray-800" htmlFor="availability">
             Disponibilidad:
@@ -102,7 +78,7 @@ const EditProduct = () => {
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
-          value="Editar Producto"
+          value="Guardar Cambios"
         />
       </Form>
     </>
--- client/src/views/NewProduct.tsx
+++ client/src/views/NewProduct.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router";
 import ErrorMessage from "../components/ErrorMessage";
 import { addProduct } from "../services/ProductsService";
+import ProductForm from "../components/ProductForm";
 export async function action({ request }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData());
   let error = "";
@@ -37,30 +38,7 @@ const NewProduct = () => {
       </div>
       {error && <ErrorMessage>{error}</ErrorMessage>}
       <Form method="post" className="mt-10">
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50 placeholder:text-gray-500"
-            placeholder="Nombre del Producto"
-            name="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50 placeholder:text-gray-500"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-          />
-        </div>
+        <ProductForm/>
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
